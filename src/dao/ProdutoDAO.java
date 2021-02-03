f750884369b144be0bbcee6190dcf81220f78ec5
leanderson <leotecinfo21@gmail.com>
2021-02-02 23:14:58 -0300

    inserindo metodo excluir

--- src/dao/ProdutoDAO.java
+++ src/dao/ProdutoDAO.java
@@ -4,11 +4,15 @@ package dao;
 import java.sql.Connection;
 import model.Produto;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.sql.Statement;
 
 public class ProdutoDAO {
     private Connection conn;
     private PreparedStatement stmt;
-    
+    private ResultSet rst;
+    private Statement smt;
     
     public ProdutoDAO(){
         conn = new ConnectionFactory().getConexao();
@@ -29,7 +33,22 @@ public class ProdutoDAO {
     
     public void alterar(Produto produto){
         try{
-            
+            stmt = conn.prepareStatement("UPDATE produto SET descricao = ?, preco = ? WHERE cod_produto = ? ");
+            stmt.setString(1, produto.getDescricao());
+            stmt.setDouble(2, produto.getPreco());
+            stmt.setInt(3, produto.getCod_produto());
+            stmt.execute();
+            stmt.close();
+        }catch(Exception erro){
+            throw new RuntimeException("erro: "+erro);
+        }
+    }
+    
+    public void excluir(int valor){
+        try{
+            smt = conn.createStatement();
+            smt.execute("DELETE FROM produto WHERE cod_produto ="+valor);
+            smt.close();
         }catch(Exception erro){
             throw new RuntimeException("erro: "+erro);
         }
