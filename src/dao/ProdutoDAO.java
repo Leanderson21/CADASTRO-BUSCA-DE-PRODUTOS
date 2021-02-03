274879dd40a14e118cf54aef5e42e366a6160b2e
leanderson <leotecinfo21@gmail.com>
2021-02-03 00:03:28 -0300

    terminando metodo select

--- src/dao/ProdutoDAO.java
+++ src/dao/ProdutoDAO.java
@@ -12,48 +12,64 @@ public class ProdutoDAO {
     private Connection conn;
     private PreparedStatement stmt;
     private ResultSet rst;
-    private Statement smt;
+    private Statement stm;
+    private ArrayList<Produto> lista = new ArrayList();
     
     public ProdutoDAO(){
         conn = new ConnectionFactory().getConexao();
     }
     
-    public void inserir(Produto produto){
-        try{
+        public void inserir(Produto produto){
+            try{
             stmt = conn.prepareStatement("INSERT INTO produto(descricao,preco) VALUES(?,?)");
             stmt.setString(1, produto.getDescricao());
             stmt.setDouble(2, produto.getPreco());
             stmt.execute();
             stmt.close();
-        }catch(Exception erro){
-            throw new RuntimeException("erro: "+erro);
-        }
+            }catch(Exception erro){
+                throw new RuntimeException("erro:"+erro);
+            }
     }
-    
-    
-    public void alterar(Produto produto){
-        try{
-            stmt = conn.prepareStatement("UPDATE produto SET descricao = ?, preco = ? WHERE cod_produto = ? ");
-            stmt.setString(1, produto.getDescricao());
-            stmt.setDouble(2, produto.getPreco());
-            stmt.setInt(3, produto.getCod_produto());
-            stmt.execute();
-            stmt.close();
-        }catch(Exception erro){
-            throw new RuntimeException("erro: "+erro);
+        
+        public void alterar(Produto produto){
+            try{
+                stmt = conn.prepareStatement("UPDATE produto SET descricao= ?, preco= ?, cod_produto= ?");
+                stmt.setString(1, produto.getDescricao());
+                stmt.setDouble(2, produto.getPreco());
+                stmt.setInt(3, produto.getCod_produto());
+                stmt.execute();
+                stmt.close();
+            }catch(Exception erro){
+                throw new RuntimeException("erro: "+erro);
+            }
         }
-    }
-    
-    public void excluir(int valor){
-        try{
-            smt = conn.createStatement();
-            smt.execute("DELETE FROM produto WHERE cod_produto ="+valor);
-            smt.close();
-        }catch(Exception erro){
-            throw new RuntimeException("erro: "+erro);
+        public void excluir(int valor){
+            try{
+                stm = conn.createStatement();
+                stm.execute("DELETE FROM produto WHERE cod_produto ="+valor);
+                stm.close();
+            }catch(Exception erro){
+                throw new RuntimeException("erro "+erro);
+            }
         }
-    }
-    
+        
+        public ArrayList<Produto> exibirTodos(){
+            try{
+                stm = conn.createStatement();
+                rst = stm.executeQuery("SELECT * FROM produto");
+                  
+                        while(rst.next()){
+                  Produto produto = new Produto();
+                        produto.setCod_produto(rst.getInt("cod_produto"));
+                        produto.setDescricao(rst.getString("descricao"));
+                        produto.setPreco(rst.getDouble("preco"));
+                        lista.add(produto);
+                        }
+            }catch(Exception erro){
+                throw new RuntimeException("erro: "+erro);
+            }
+            return lista;
+        }
     
 }
 
@@ -88,4 +104,30 @@ public class ProdutoDAO {
             throw new RuntimeException("erro: "+erro);
             }
     }
+
+public void alterar(Produto produto){
+        try{
+            stmt = conn.prepareStatement("UPDATE produto SET descricao = ?, preco = ? WHERE cod_produto = ? ");
+            stmt.setString(1, produto.getDescricao());
+            stmt.setDouble(2, produto.getPreco());
+            stmt.setInt(3, produto.getCod_produto());
+            stmt.execute();
+            stmt.close();
+        }catch(Exception erro){
+            throw new RuntimeException("erro: "+erro);
+        }
+    }
+    
+    public void excluir(int valor){
+        try{
+            smt = conn.createStatement();
+            smt.execute("DELETE FROM produto WHERE cod_produto ="+valor);
+            smt.close();
+        }catch(Exception erro){
+            throw new RuntimeException("erro: "+erro);
+        }
+    }
+
+
+
 } */
