ad444df4f918c40df9ba0f9764d876bfa7b662e0
leanderson <leotecinfo21@gmail.com>
2021-02-04 18:23:49 -0300

    inserindo TableModel

--- src/dao/ProdutoDAO.java
+++ src/dao/ProdutoDAO.java
@@ -11,65 +11,64 @@ import java.sql.Statement;
 public class ProdutoDAO {
     private Connection conn;
     private PreparedStatement stmt;
-    private ResultSet rst;
     private Statement stm;
+    private ResultSet rst;
     private ArrayList<Produto> lista = new ArrayList();
     
-    public ProdutoDAO(){
-        conn = new ConnectionFactory().getConexao();
-    }
     
-        public void inserir(Produto produto){
-            try{
-            stmt = conn.prepareStatement("INSERT INTO produto(descricao,preco) VALUES(?,?)");
-            stmt.setString(1, produto.getDescricao());
-            stmt.setDouble(2, produto.getPreco());
-            stmt.execute();
-            stmt.close();
-            }catch(Exception erro){
-                throw new RuntimeException("erro:"+erro);
-            }
-    }
+    
+    public void inserir(Produto produto){
+        try{
+        stmt = conn.prepareStatement("INSERT INTO produto(descricao,preco) VALUES(?,?)");
+        stmt.setString(1, produto.getDescricao());
+        stmt.setDouble(2, produto.getPreco());
+        stmt.execute();
+        stmt.close();
+        }catch(Exception erro){
+            throw new RuntimeException("erro 1"+erro);
+        }
         
         public void alterar(Produto produto){
             try{
-                stmt = conn.prepareStatement("UPDATE produto SET descricao= ?, preco= ?, cod_produto= ?");
+                stmt = conn.prepareStatement("UPDATE produto SET descricao =? , preco = ? WHERE cod_produto = ?");
                 stmt.setString(1, produto.getDescricao());
-                stmt.setDouble(2, produto.getPreco());
+                stmt.setDoule(2, produto.getPreco());
                 stmt.setInt(3, produto.getCod_produto());
                 stmt.execute();
                 stmt.close();
             }catch(Exception erro){
-                throw new RuntimeException("erro: "+erro);
-            }
-        }
-        public void excluir(int valor){
-            try{
-                stm = conn.createStatement();
-                stm.execute("DELETE FROM produto WHERE cod_produto ="+valor);
-                stm.close();
-            }catch(Exception erro){
-                throw new RuntimeException("erro "+erro);
+                throw new RuntimeException("erro 2:"+erro);
             }
         }
         
-        public ArrayList<Produto> exibirTodos(){
+        public void deletar(int valor){
+            try{
+                stm = conn.createStatement();
+                stm.execute("DELETE FROM produto WHERE cod_produto="+valor);
+                stm.close();
+            }catch(Exception erro){
+                throw new RuntimeException("erro 3:"+erro);
+            }
+        }
+        
+        public  ArrayList<Produto> listarTodos(){
             try{
                 stm = conn.createStatement();
                 rst = stm.executeQuery("SELECT * FROM produto");
-                  
-                        while(rst.next()){
-                  Produto produto = new Produto();
-                        produto.setCod_produto(rst.getInt("cod_produto"));
-                        produto.setDescricao(rst.getString("descricao"));
-                        produto.setPreco(rst.getDouble("preco"));
-                        lista.add(produto);
-                        }
+                    while(rst.next()){
+                        Produto produto = new Produto();
+                            produto.setCod_produto(rst.getInt("cod_produto"));
+                            produto.setDescricao(rst.getString("descricao"));
+                            produto.setPreco(rst.getDouble("preco"));
+                            lista.add(produto);
+                    }
             }catch(Exception erro){
-                throw new RuntimeException("erro: "+erro);
+                throw new RuntimeException("erro 4:"+erro);
             }
             return lista;
         }
+        
+    }
     
 }
 
@@ -82,10 +81,15 @@ import java.sql.Connection;
 import model.Produto;
 import java.sql.PreparedStatement;
 
+
 public class ProdutoDAO {
     
     private Connection conn;
     private PreparedStatement stmt;
+   private ResultSet rst;
+    private Statement stm;
+    private ArrayList<Produto> lista = new ArrayList();
+    
     
     
     public ProdutoDAO(){
@@ -128,6 +132,24 @@ public void alterar(Produto produto){
         }
     }
 
+
+ public ArrayList<Produto> exibirTodos(){
+            try{
+                stm = conn.createStatement();
+                rst = stm.executeQuery("SELECT * FROM produto");
+                  
+                        while(rst.next()){
+                        Produto produto = new Produto();
+                        produto.setCod_produto(rst.getInt("cod_produto"));
+                        produto.setDescricao(rst.getString("descricao"));
+                        produto.setPreco(rst.getDouble("preco"));
+                        lista.add(produto);
+                        }
+                        }catch(Exception erro){
+                        throw new RuntimeException("erro: "+erro);
+                        }
+                        return lista;
+        }
 
 
 } */
--- src/table/ProdutoTableModel.java
+++ src/table/ProdutoTableModel.java
@@ -1,7 +1,48 @@
 
 package table;
+import model.Produto;
+import javax.swing.table.AbstractTableModel;
+import java.util.ArrayList;
 
 
-public class ProdutoTableModel {
+public class ProdutoTableModel extends AbstractTableModel {
+
+    private static final int COL_COD_PRODUTO = 0;
+    private static final int COL_DESCRICAO = 1;
+    private static final int COL_PRECO = 2;
+    private ArrayList<Produto> lista;
+    
+    
+    public ProdutoTableModel(ArrayList<Produto> l){
+        lista = new ArrayList<Produto>(l);
+    }
+    
+    @Override
+    public int getRowCount() {
+        return lista.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 3;
+    }
+
+    @Override
+    public Object getValueAt(int linhas, int colunas) {
+        Produto produto = lista.get(linhas);
+        if (colunas == COL_COD_PRODUTO) return produto.getCod_produto();
+        if (colunas == COL_DESCRICAO) return produto.getDescricao();
+        if (colunas == COL_PRECO) return produto.getPreco();
+        return "";
+        
+    }
+    
+    @Override
+        public String getColumnName(int colunas){
+        if (colunas == COL_COD_PRODUTO) return "código";
+        if (colunas == COL_DESCRICAO) return "descricao";
+        if (colunas == COL_PRECO) return "preco";
+        return "";
+        }
     
 }
